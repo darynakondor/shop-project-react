12f1fb6b1f4141fb5a9a4ea3f5b56ea6bdf8da8a
Daryna <condordaryna@gmail.com>
2022-08-10 20:11:13 +0300

    add mui to project

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -18,29 +18,10 @@
 
 import React from 'react'
 
-type Props = {
-    title: string
-}
-
-const AppList = () => {
-    return (
-        <ul>
-            <li>list item 1</li>
-            <li>list item 2</li>
-            <li>list item 3</li>
-        </ul>
-    )
-}
-
-const AppHeader = (props: Props) => {
-    return <h1>Hello {props.title}</h1>
-}
 const App = () => {
   return (
     <>
-        <AppHeader title="App.js"/>
-        <AppHeader title="React.js"/>
-        <AppList />
+        <h1>Hello world</h1>
     </>
   )
 }
