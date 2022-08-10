04a406f54185ed6af3f3405825ea51517f431f48
Daryna <condordaryna@gmail.com>
2022-08-10 18:38:12 +0300

    add test App

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const h1 = <h1>Hello world</h1>
+
+function App() {
+    return (
+        <div>
+            {h1}
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores voluptate possimus amet animi quia non.</p>
+        </div>
+    )
+}
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-root.render("Hello world");
+root.render(<App />);
