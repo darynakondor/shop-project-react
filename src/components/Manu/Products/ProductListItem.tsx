6a35e794e249dd5506a0cf251872fd6d6409f029
Daryna <condordaryna@gmail.com>
2022-08-29 03:10:21 +0300

    add onIncrementClick, onDecrementClick and toggleChangeColor

--- src/components/Manu/Products/ProductListItem.tsx
+++ src/components/Manu/Products/ProductListItem.tsx
@@ -1,5 +1,6 @@
 import { Button, Card, CardActions, CardContent, TextField } from '@mui/material'
-import React from 'react'
+import { green } from '@mui/material/colors'
+import React, { useState } from 'react'
 import './ProductListItem.scss'
 
 export type ProductProps = {
@@ -21,29 +22,38 @@ const ProductListItem = ({
     image
 
 }: ProductProps) => {
+    const [count, setCount] = useState<number>(1)
 
-  return (
-    <Card>
-        <CardContent>
-            <div className='product-img'>
-                <img src={image} alt="" />
-            </div>
-            <h3 className='product-title'>{name}</h3>
-            <div className="product-description">{description}</div>
-            <div className="product-features">Type: {type}</div>
-            <div className="product-features">Capacity: {capacity}Gb</div>
-            <div className="product-price">$ {price}</div>
-            <div className="product-quantity">
-                <Button variant='contained'>-</Button>
-                <TextField size='small' value={1} variant='outlined' />
-                <Button variant='contained'>+</Button>
-            </div>
-        </CardContent>
-        <CardActions className='btn-wrap'>
-            <Button variant='outlined'>Add to cart</Button>
-        </CardActions>
-    </Card>
-  )
+    const onIncrementClick = () => (setCount((prevState:number) => prevState + 1))
+    const onDecrementClick = () => (setCount((prevState:number) => prevState - 1))
+
+    const [color, setColor] = useState<string>('green')
+    const toggleChangeColor = () => (setColor((prevState:string) => prevState === 'red' ? 'green' : 'red'))
+
+    return (
+        <Card>
+            <CardContent>
+                <div className='product-img'>
+                    <img src={image} alt="" />
+                </div>
+                <h3 className='product-title'>{name}</h3>
+                <div className="product-description">{description}</div>
+                <div className="product-features">Type: {type}</div>
+                <div className="product-features">Capacity: {capacity}Gb</div>
+                <div className="product-price">$ {price}</div>
+                <div className="product-quantity">
+                    <Button variant='contained' onClick={onDecrementClick}>-</Button>
+                    <TextField size='small' value={count} variant='outlined' />
+                    <Button variant='contained' onClick={onIncrementClick}>+</Button>
+                </div>
+                <p>Color: {color}</p>
+                <button onClick={toggleChangeColor}>Change color</button>
+            </CardContent>
+            <CardActions className='btn-wrap'>
+                <Button variant='outlined'>Add to cart</Button>
+            </CardActions>
+        </Card>
+    )
 }
 
 export default ProductListItem
