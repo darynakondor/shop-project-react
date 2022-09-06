cb52bb819b73ee1c54180f056b3b16eed897a1d7
Daryna <condordaryna@gmail.com>
2022-09-06 22:36:13 +0300

    add CartProductList and change CartPage, CartHeader

---
+++ src/components/Cart/CartProductList.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {keys} from 'lodash'
+import productsArray, { getProductsObject, Product } from '../Manu/Products/productsArray'
+
+type Props = {
+    productsInCart: {
+        [id: number]: number,
+    }
+    productsObject?: {
+        [id: number]: Product
+    }
+}
+
+const CartProductList = ({
+    productsInCart, productsObject = getProductsObject(productsArray),
+}: Props) => {
+  return (
+    <div>
+        {keys(productsInCart).map((productId) => (
+            <div   key={productId}>
+
+                {productsObject[parseInt(productId)].name}:
+                <span style={{ marginLeft: "5px", fontWeight: "700", color: "rgb(255, 139, 400)"}}>{productsInCart[parseInt(productId)]}</span> 
+
+            </div>
+        ))}
+    </div>
+  )
+}
+
+export default CartProductList
--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,31 +1,18 @@
 import React from 'react'
-import {keys} from 'lodash'
-import productsArray, { getProductsObject, Product } from '../../components/Manu/Products/productsArray'
 import CartTotal from 'components/Cart/CartTotal'
+import CartProductList from 'components/Cart/CartProductList'
 
 type Props = {
   productsInCart: {
       [id: number]: number,
   }
-  productsObject?: {
-      [id: number]: Product
-  }
 }
 
 const CartPage = ({
-  productsInCart, productsObject = getProductsObject(productsArray),
-}: Props) => {
+  productsInCart}: Props) => {
   return (
       <div>
-          {keys(productsInCart).map((productId) => (
-          <div   key={productId}>
-
-              {productsObject[parseInt(productId)].name}:
-              <span style={{ marginLeft: "5px", fontWeight: "700", color: "rgb(255, 139, 400)"}}>{productsInCart[parseInt(productId)]}</span>: {' '} {productsObject[parseInt(productId)].price}
-
-          </div>
-             
-          ))}
+          <CartProductList productsInCart={productsInCart}/>
           <CartTotal productsInCart={productsInCart} />
       </div>
   )
